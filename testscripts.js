247e26278b6fd13833d80dbb172d04fb3c059536
Patrick Skelly <pskelly91@gmail.com>
2014-07-29 13:11:53 -0400

    im stupid and called getdate() instead of grabDate()

--- testscripts.js
+++ testscripts.js
@@ -2,3 +2,77 @@ function getDate() {
     "use strict";
     document.getElementById("date").innerHTML = Date();
 }
+
+function grabDate() {
+    "use strict";
+    var day = "day of the week";
+    var month;
+    var year = new Date().getFullYear;
+    var numDay;
+    
+    switch (new Date().getDay()) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+    }
+    
+    switch (new Date().getMonth()) {
+        case 0:
+            month = "January";
+            break;
+        case 1:
+            month = "February";
+            break;
+        case 2:
+            month = "March";
+            break;
+        case 3:
+            month = "April";
+            break;
+        case 4:
+            month = "May";
+            break;
+        case 5:
+            month = "June";
+            break;
+        case 6:
+            month = "July";
+            break;
+        case 7:
+            month = "August";
+            break;
+        case 8:
+            month = "September";
+            break;
+        case 9:
+            month = "October";
+            break;
+        case 10:
+            month = "November";
+            break;
+        case 11:
+            month = "December";
+            break;
+    }
+        
+    document.getElementById("date").innerHTML = day + month + year;
+    
+}
