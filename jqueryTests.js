96b5a471268972f116db3383a3adbf04e7e5f2ab
Patrick Skelly <pskelly91@gmail.com>
2014-08-06 11:43:34 -0400

    banner changes but cant cycle

--- jqueryTests.js
+++ jqueryTests.js
@@ -6,4 +6,27 @@ $(document).ready(function() {
     $hide.mouseover(function(){
         $hide.show();
     });
+    
+    
+    
+    var $originalColor = true;
+    
+    if ($originalColor == true){
+        $("#banner").click(function() {
+            $(this).css("background", "linear-gradient(to bottom, #990000, #FFFFFF)")
+            $(this).css("background", "-moz-linear-gradient(to bottom, #990000, #FFFFFF)")
+            $(this).css("background", "-o-linear-gradient(to bottom, #990000, #FFFFFF)")
+            $(this).css("background", "-webkit-linear-gradient(to bottom, #990000, #FFFFFF)");
+            $originalColor = false;
+        });
+    }else if ($originalColor == false){
+        $("#banner").click(function() {
+            $(this).css("background", "linear-gradient(to bottom, #336699, #FFFFFF)")
+            $(this).css("background", "-moz-linear-gradient(to bottom, #336699, #FFFFFF)")
+            $(this).css("background", "-o-linear-gradient(to bottom, #336699, #FFFFFF)")
+            $(this).css("background", "-webkit-linear-gradient(to bottom, #336699, #FFFFFF)");
+            $originalColor = true;
+        });
+    }
 });
+
