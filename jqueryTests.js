c86bd3f3f43e06b8471bcd57e3c939534ce8071c
Patrick Skelly <pskelly91@gmail.com>
2014-08-06 15:17:33 -0400

    finally can toggle the banner color

--- jqueryTests.js
+++ jqueryTests.js
@@ -1,32 +1,27 @@
 $(document).ready(function() {
     
     var $hide = $('.hiding');
-    $hide.hide(10000);
     
-    $hide.mouseover(function(){
+    $hide.click(function(){
+    $hide.hide(10000);
+    });
+    
+    $hide.click(function(){
         $hide.show();
     });
     
     
-    
-    var $originalColor = true;
-    
-    if ($originalColor == true){
-        $("#banner").click(function() {
-            $(this).css("background", "linear-gradient(to bottom, #990000, #FFFFFF)")
-            $(this).css("background", "-moz-linear-gradient(to bottom, #990000, #FFFFFF)")
-            $(this).css("background", "-o-linear-gradient(to bottom, #990000, #FFFFFF)")
-            $(this).css("background", "-webkit-linear-gradient(to bottom, #990000, #FFFFFF)");
-            $originalColor = false;
-        });
-    }else if ($originalColor == false){
-        $("#banner").click(function() {
-            $(this).css("background", "linear-gradient(to bottom, #336699, #FFFFFF)")
-            $(this).css("background", "-moz-linear-gradient(to bottom, #336699, #FFFFFF)")
-            $(this).css("background", "-o-linear-gradient(to bottom, #336699, #FFFFFF)")
-            $(this).css("background", "-webkit-linear-gradient(to bottom, #336699, #FFFFFF)");
-            $originalColor = true;
-        });
-    }
+    $(".banner").click(function() {
+       
+        $(this).toggleClass("bannerTwo").toggleClass("banner");
+       /* $(this).css("background", "linear-gradient(to bottom, #990000, #FFFFFF)")
+        $(this).css("background", "-moz-linear-gradient(to bottom, #990000, #FFFFFF)")
+        $(this).css("background", "-o-linear-gradient(to bottom, #990000, #FFFFFF)")
+        $(this).css("background", "-webkit-linear-gradient(to bottom, #990000, #FFFFFF)");
+        $(".sidebar").css("background-color", "#CCCC99");
+            */
+        
+    });
+
 });
 
